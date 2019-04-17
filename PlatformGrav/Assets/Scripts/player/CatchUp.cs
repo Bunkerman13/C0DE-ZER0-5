917684bc7b9314c96a9ff0ae1824c9aa67e05147
ebd1998 <ejp4604@rit.edu>
2019-04-17 09:50:05 -0400

    Basis for catchup mechanic if player object gets knocked back.

---
+++ PlatformGrav/Assets/Scripts/player/CatchUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchUp : MonoBehaviour
+{
+    float catchUpValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        catchUpValue = GameObject.Find("Player").GetComponent<Transform>().position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (gameObject.transform.position.x < catchUpValue)
+            //transform.Translate(Vector3.right * 1f * Time.deltaTime);
+    }
+}
