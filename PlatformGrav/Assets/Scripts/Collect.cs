bdec0f1789a9d65df0abac0e4db7ad7b230d74df
wts6374 <wts6374@g.rit.edu>
2019-04-19 16:26:46 -0400

    I got the rotation and long platforms in, adjusted the balance and added SFX and music again. Let me know if anything's wrong. Check branch WTSnewStuff

--- PlatformGrav/Assets/Scripts/Collect.cs
+++ PlatformGrav/Assets/Scripts/Collect.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Collect : MonoBehaviour
 {
-    
+    public AudioSource coin;
+    public AudioSource hit;
+    public AudioSource pick1;
+    public AudioSource pick2;
+    public AudioSource pick3;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -23,17 +27,19 @@ public class Collect : MonoBehaviour
                 gameObject.GetComponent<SlowDown>().isActive = true;
                 gameObject.GetComponent<SpriteRenderer>().enabled = false;
                 gameObject.transform.position = new Vector2(-100, -100);
+                pick3.Play();
             }
             else if (gameObject.tag == "RainCoin")
             {
                 gameObject.GetComponent<RainCoin>().isActive = true;
                 gameObject.GetComponent<SpriteRenderer>().enabled = false;
                 gameObject.transform.position = new Vector2(-100, -100);
+                pick2.Play();
             }
             else if (gameObject.tag == "Coin")
             {
 
-
+                coin.Play();
                 MySceneManager.Instance.score += 10;
                 MySceneManager.Instance.multiplier += .1f;
                 MySceneManager.Instance.coins++;
@@ -48,6 +54,7 @@ public class Collect : MonoBehaviour
                 //Physics.gravity = new Vector3(0, -50, 0);
                 Physics2D.gravity = new Vector2(0, -50f);
                 other.gameObject.GetComponent<MainMenu>().Menuetwo();
+                hit.Play();
             }
             else if (gameObject.tag == "Mag")
             {
@@ -55,6 +62,7 @@ public class Collect : MonoBehaviour
 				gameObject.GetComponent<coinMagnet> ().duration = 10;
 				gameObject.GetComponent<SpriteRenderer>().enabled = false;
 				gameObject.transform.position = new Vector2(-100, -100);
+                pick1.Play();
                 //Destroy(gameObject);
             }
             
---
+++ PlatformGrav/Assets/Scripts/Spin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public bool stop;
+    public static float SPEED;
+    public float speed;
+    public float rotateSpeed=1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SPEED < 20)
+        {
+            SPEED = 20f;
+        }
+
+        speed = SPEED;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left * (rotateSpeed * Time.deltaTime));
+        speed = SPEED;
+        transform.Translate(Vector3.left * SPEED * Time.deltaTime);
+    }
+}
