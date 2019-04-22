6cc6f3888c4a231cdfb48e1ad807bd3e77814567
wts6374 <wts6374@g.rit.edu>
2019-04-22 15:50:31 -0400

    I got rotating platforms!!!!

--- PlatformGrav/Assets/Scripts/Spin.cs
+++ PlatformGrav/Assets/Scripts/Spin.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    public bool stop;
-    public static float SPEED;
-    public float speed;
-    public float rotateSpeed=1.0f;
+    public float rotateSpeed=10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (SPEED < 20)
-        {
-            SPEED = 20f;
-        }
 
-        speed = SPEED;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.left * (rotateSpeed * Time.deltaTime));
-        speed = SPEED;
-        transform.Translate(Vector3.left * SPEED * Time.deltaTime);
+        transform.Rotate(Vector3.back* rotateSpeed * Time.deltaTime);
     }
 }
