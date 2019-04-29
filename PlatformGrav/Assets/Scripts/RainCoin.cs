ad2b4cdac30b6f6fc97ba406241919e2a1adf204
ebd1998 <ejp4604@rit.edu>
2019-04-29 09:53:37 -0400

    Fix coin issue

--- PlatformGrav/Assets/Scripts/RainCoin.cs
+++ PlatformGrav/Assets/Scripts/RainCoin.cs
@@ -52,8 +52,8 @@ public class RainCoin : PowerUp {
                     tempColor.a -= .1f;
                     g.GetComponent<SpriteRenderer>().color = tempColor;
 
-                    // Destroy(g, 3);
-                    g.SetActive(false);
+                    Destroy(g, 3);
+                    //g.SetActive(false);
                 }
             }
         }
